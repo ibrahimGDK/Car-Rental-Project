51d4db4a175ecb430f1bdc8e1f5eb5e7830c2269
ibrahimGDK <ibrgdk@gmail.com>
2025-05-12 20:11:30 +0300

    hata düzeltildi

--- Backend/carRentalProject/src/main/java/com/iuc/entities/User.java
+++ Backend/carRentalProject/src/main/java/com/iuc/entities/User.java
@@ -45,7 +45,7 @@ public class User {
     private Boolean emailVerified = false;
 
     @Column(nullable = false)
-    private Boolean builtIn = true ; // silinmemesi gereken user için bunu yaptık. dafault olarak false yaptık. ancak ilk admin
+    private Boolean builtIn = false ; // silinmemesi gereken user için bunu yaptık. dafault olarak false yaptık. ancak ilk admin
     // oluşturulurken bu özelliği TRUE yaparız. Bunu da bussiness logic olarak yapacağız
 // builtIn= yerleşik (örneğin, silinmemesi gereken bir yönetici) olup olmadığını belirlemek için kullanılır
     @ManyToMany // LAZY

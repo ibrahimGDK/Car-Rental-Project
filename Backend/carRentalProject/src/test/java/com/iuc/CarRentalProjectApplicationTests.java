f41d759c96ee69c23066d01eb750d94dbcbdb759
ibrahimGDK <ibrgdk@gmail.com>
2025-05-30 16:32:02 +0300

    dev_user doesn't exist sorunu halledildi

--- Backend/carRentalProject/src/test/java/com/iuc/CarRentalProjectApplicationTests.java
+++ Backend/carRentalProject/src/test/java/com/iuc/CarRentalProjectApplicationTests.java
@@ -2,7 +2,9 @@ package com.iuc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CarRentalProjectApplicationTests {
 
